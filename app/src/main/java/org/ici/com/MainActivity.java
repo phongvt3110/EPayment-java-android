f8182bf85f118dccae1f80a2c6f5bda85bece174
phongvt3110 <vtphong651043@gmail.com>
2019-10-04 14:11:54 +0700

    bai21-22: Random number

--- app/src/main/java/org/ici/com/MainActivity.java
+++ app/src/main/java/org/ici/com/MainActivity.java
@@ -9,25 +9,49 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
     TextView txtNoidung;
     Button btnClick;
-    EditText editText;
+    EditText userName;
+    Button btnGen;
+    EditText minText;
+    EditText maxText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        txtNoidung = (TextView) findViewById(R.id.txtNoidung);
-        txtNoidung.setText("Đăng nhập hệ thống");
-        btnClick = (Button) findViewById(R.id.btnClick);
-        editText = (EditText) findViewById(R.id.username);
-        editText.setText("Android Khoa Pham");
+        init();
         btnClick.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 txtNoidung.setText("Đang xử lý ...");
-                Toast.makeText(MainActivity.this, editText.getText().toString(), Toast.LENGTH_SHORT).show();
             }
         });
+        btnGen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String ranNum = String.valueOf(randomnum(minText.getText().toString(), maxText.getText().toString()));
+                userName.setText(ranNum);
+            }
+        });
+    }
+
+    private void init() {
+        txtNoidung = (TextView) findViewById(R.id.txtNoidung);
+        btnClick = (Button) findViewById(R.id.btnClick);
+        btnGen = findViewById(R.id.btnGen);
+        userName = (EditText) findViewById(R.id.username);
+        minText = findViewById(R.id.min);
+        maxText = findViewById(R.id.max);
+        userName.setText("Android Khoa Pham");
+    }
+    private int randomnum(String min, String max){
+        Random random = new Random();
+        int minnum = Integer.parseInt(min);
+        int maxnum = Integer.parseInt(max);
+        int rannum = random.nextInt(maxnum - minnum + 1) + minnum;
+        return rannum;
     }
 }

92ffd620c241847d27f20109602f1cea0c1d9fcb
phongvt3110 <vtphong651043@gmail.com>
2019-10-04 12:58:22 +0700

    update

--- app/src/main/java/org/ici/com/MainActivity.java
+++ app/src/main/java/org/ici/com/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         btnClick.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                txtNoidung.setText("Lập trình Android");
+                txtNoidung.setText("Đang xử lý ...");
             }
         });
     }

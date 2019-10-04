ff72e8efbeb233d4122f90dff5ee43e34c03f10e
phongvt3110 <vtphong651043@gmail.com>
2019-10-04 13:21:38 +0700

    bai19: edit text

--- app/src/main/java/org/ici/com/MainActivity.java
+++ app/src/main/java/org/ici/com/MainActivity.java
@@ -5,11 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     TextView txtNoidung;
     Button btnClick;
+    EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,10 +20,13 @@ public class MainActivity extends AppCompatActivity {
         txtNoidung = (TextView) findViewById(R.id.txtNoidung);
         txtNoidung.setText("Đăng nhập hệ thống");
         btnClick = (Button) findViewById(R.id.btnClick);
+        editText = (EditText) findViewById(R.id.username);
+        editText.setText("Android Khoa Pham");
         btnClick.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 txtNoidung.setText("Đang xử lý ...");
+                Toast.makeText(MainActivity.this, editText.getText().toString(), Toast.LENGTH_SHORT).show();
             }
         });
     }

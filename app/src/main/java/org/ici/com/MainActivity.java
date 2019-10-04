633709c0e40da47621004f2d395ca05b84f5989f
phongvt3110 <vtphong651043@gmail.com>
2019-10-04 11:12:10 +0700

    bai 18

--- app/src/main/java/org/ici/com/MainActivity.java
+++ app/src/main/java/org/ici/com/MainActivity.java
@@ -3,16 +3,25 @@ package org.ici.com;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView txtNoidung;
+    Button btnClick;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String hoten = "Vũ Tùng Phong";
-        int nam = 2019;
-        Log.d("KhoaPham", hoten + "-" + nam);
+        txtNoidung = (TextView) findViewById(R.id.txtNoidung);
+        txtNoidung.setText("Đăng nhập hệ thống");
+        btnClick = (Button) findViewById(R.id.btnClick);
+        btnClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txtNoidung.setText("Lập trình Android");
+            }
+        });
     }
 }

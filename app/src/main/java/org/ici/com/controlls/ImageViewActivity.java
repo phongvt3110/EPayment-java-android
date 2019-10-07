0ff41bf04b197df1d1b89a696e2c7af0de0db9b2
phongvt3110 <vtphong651043@gmail.com>
2019-10-07 16:56:53 +0700

    random screen

--- app/src/main/java/org/ici/com/controlls/ImageViewActivity.java
+++ app/src/main/java/org/ici/com/controlls/ImageViewActivity.java
@@ -10,11 +10,15 @@ import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class ImageViewActivity extends AppCompatActivity {
     ImageView imageView;
     RelativeLayout relativeLayout;
     Button changeBackground;
     ImageButton imageButton;
+    ArrayList<Integer> arrayImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,18 +26,24 @@ public class ImageViewActivity extends AppCompatActivity {
         imageView = (ImageView) findViewById(R.id.imageView);
         imageView.setImageResource(R.drawable.penguin);
         changeBackground = (Button) findViewById(R.id.btnChangeBackground);
+        relativeLayout = (RelativeLayout)findViewById(R.id.imageViewScreen);
         changeBackground.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                relativeLayout = (RelativeLayout)findViewById(R.id.imageViewScreen);
                 relativeLayout.setBackgroundResource(R.drawable.images1);
             }
         });
         imageButton = (ImageButton) findViewById(R.id.imageButton);
+        arrayImage = new ArrayList<>();
+        arrayImage.add(R.drawable.images);
+        arrayImage.add(R.drawable.images1);
+        arrayImage.add(R.drawable.image3);
         imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(ImageViewActivity.this, "ImageButtonClick", Toast.LENGTH_SHORT).show();
+                Random random = new Random();
+                int vitri = random.nextInt(arrayImage.size());
+                relativeLayout.setBackgroundResource(arrayImage.get(vitri));
             }
         });
     }

9d9bf88063b249e89f8430ea9bab66a08b9b3cab
phongvt3110 <vtphong651043@gmail.com>
2019-10-07 16:36:08 +0700

    ImageButton

--- app/src/main/java/org/ici/com/controlls/ImageViewActivity.java
+++ app/src/main/java/org/ici/com/controlls/ImageViewActivity.java
@@ -5,13 +5,16 @@ import org.ici.com.R;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 public class ImageViewActivity extends AppCompatActivity {
     ImageView imageView;
     RelativeLayout relativeLayout;
     Button changeBackground;
+    ImageButton imageButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +27,13 @@ public class ImageViewActivity extends AppCompatActivity {
             public void onClick(View view) {
                 relativeLayout = (RelativeLayout)findViewById(R.id.imageViewScreen);
                 relativeLayout.setBackgroundResource(R.drawable.images1);
+            }
+        });
+        imageButton = (ImageButton) findViewById(R.id.imageButton);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(ImageViewActivity.this, "ImageButtonClick", Toast.LENGTH_SHORT).show();
             }
         });
     }

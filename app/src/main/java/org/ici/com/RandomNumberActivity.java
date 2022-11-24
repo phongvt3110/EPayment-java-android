af5dd9be5107238e1cf190ad7184f9c5df2d1e74
phongvt3110 <vtphong651043@gmail.com>
2022-11-24 23:09:51 +0700

    update gradle version

--- app/src/main/java/org/ici/com/RandomNumberActivity.java
+++ app/src/main/java/org/ici/com/RandomNumberActivity.java
@@ -1,13 +1,14 @@
 package org.ici.com;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Random;
 
@@ -22,37 +23,41 @@ public class RandomNumberActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_random_number);
         init();
-        btnGen.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String chuoiMin = minText.getText().toString();
-                String chuoiMax = maxText.getText().toString();
-                if(chuoiMin.isEmpty() || chuoiMax.isEmpty()) {
-                    Toast.makeText(RandomNumberActivity.this, "Please enter all number needed", Toast.LENGTH_SHORT).show();
+        btnGen.setOnClickListener(view -> {
+            String chuoiMin = minText.getText().toString();
+            String chuoiMax = maxText.getText().toString();
+            if(chuoiMin.isEmpty() || chuoiMax.isEmpty()) {
+                Toast.makeText(RandomNumberActivity.this, "Please enter all number needed", Toast.LENGTH_SHORT).show();
+            } else {
+                int minNum = Integer.parseInt(chuoiMin);
+                int maxNum = Integer.parseInt(chuoiMax);
+                if(maxNum - minNum > 0) {
+                    String ranNum = String.valueOf(genRandomNumber(minNum, maxNum));
+                    result.setText(ranNum);
                 } else {
-                    int minNum = Integer.parseInt(chuoiMin);
-                    int maxNum = Integer.parseInt(chuoiMax);
-                    if(maxNum - minNum > 0) {
-                        String ranNum = String.valueOf(randomnum(minNum, maxNum));
-                        result.setText(ranNum);
-                    } else {
-                        Toast.makeText(RandomNumberActivity.this, "Min number equals to max number", Toast.LENGTH_SHORT).show();
-                    }
+                    Toast.makeText(RandomNumberActivity.this, "Min number equals to max number", Toast.LENGTH_SHORT).show();
                 }
             }
         });
     }
 
     private void init() {
+        findViewById(R.id.android);
         result = findViewById(R.id.textView);
         btnGen = findViewById(R.id.btnGen);
         minText = findViewById(R.id.min);
         maxText = findViewById(R.id.max);
+        String str = getAppName(this.getApplicationContext());
+        this.setTitle("Random number generator");
     }
 
-    private int randomnum(int min, int max){
+    private int genRandomNumber(int min, int max){
         Random random = new Random();
-        int rannum = random.nextInt(max - min + 1) + min;
-        return rannum;
+        return random.nextInt(max - min + 1) + min;
+    }
+    private String getAppName(Context context){
+        ApplicationInfo applicationInfo = context.getApplicationInfo();
+        int appNameId = applicationInfo.labelRes;
+        return appNameId == 0 ? applicationInfo.nonLocalizedLabel.toString() : context.getString(appNameId);
     }
 }
--- app/src/main/java/org/ici/com/controlls/ImageViewActivity.java
+++ app/src/main/java/org/ici/com/controlls/ImageViewActivity.java
@@ -1,12 +1,9 @@
 package org.ici.com.controlls;
 
-import androidx.appcompat.app.AppCompatActivity;
-import org.ici.com.R;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.CompoundButton;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.RadioButton;
@@ -14,6 +11,10 @@ import android.widget.RadioGroup;
 import android.widget.RelativeLayout;
 import android.widget.Switch;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.ici.com.R;
 
 import java.util.ArrayList;
 import java.util.Random;
@@ -34,89 +35,70 @@ public class ImageViewActivity extends AppCompatActivity {
     RadioButton rbSang;
     RadioButton rbTrua;
     RadioButton rbChieu;
+    @SuppressLint("NonConstantResourceId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_view);
-        imageView = (ImageView) findViewById(R.id.imageView);
+        imageView = findViewById(R.id.imageView);
         imageView.setImageResource(R.drawable.penguin);
-        changeBackground = (Button) findViewById(R.id.btnChangeBackground);
-        relativeLayout = (RelativeLayout)findViewById(R.id.imageViewScreen);
-        changeBackground.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                relativeLayout.setBackgroundResource(R.drawable.images1);
-            }
-        });
-        imageButton = (ImageButton) findViewById(R.id.imageButton);
+        changeBackground = findViewById(R.id.btnChangeBackground);
+        relativeLayout = findViewById(R.id.imageViewScreen);
+        changeBackground.setOnClickListener(view -> relativeLayout.setBackgroundResource(R.drawable.images1));
+        imageButton = findViewById(R.id.imageButton);
         arrayImage = new ArrayList<>();
         arrayImage.add(R.drawable.images);
         arrayImage.add(R.drawable.images1);
         arrayImage.add(R.drawable.image3);
-        imageButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Random random = new Random();
-                int vitri = random.nextInt(arrayImage.size());
-                relativeLayout.setBackgroundResource(arrayImage.get(vitri));
-            }
+        imageButton.setOnClickListener(view -> {
+            Random random = new Random();
+            int vitri = random.nextInt(arrayImage.size());
+            relativeLayout.setBackgroundResource(arrayImage.get(vitri));
         });
         swWifi = findViewById(R.id.swWifi);
-        swWifi.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                if(b) {
-                    Toast.makeText(ImageViewActivity.this, "Switch ON", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(ImageViewActivity.this, "Switch Off", Toast.LENGTH_SHORT).show();
-                }
+        swWifi.setOnCheckedChangeListener((compoundButton, b) -> {
+            if(b) {
+                Toast.makeText(ImageViewActivity.this, "Switch ON", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(ImageViewActivity.this, "Switch Off", Toast.LENGTH_SHORT).show();
             }
         });
         android = findViewById(R.id.android);
         ios = findViewById(R.id.ios);
         php = findViewById(R.id.php);
         BtnCheckbox = findViewById(R.id.btnCheckbox);
-        BtnCheckbox.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String str = "You checked: \n";
-                if(android.isChecked()){
-                    str += android.getText() + "\n";
-                }
-                if(ios.isChecked()){
-                    str += ios.getText() + "\n";
-                }
-                if(php.isChecked()){
-                    str += php.getText();
-                }
-                Toast.makeText(ImageViewActivity.this, str, Toast.LENGTH_SHORT).show();
+        BtnCheckbox.setOnClickListener(view -> {
+            String str = "You checked: \n";
+            if(android.isChecked()){
+                str += android.getText() + "\n";
             }
+            if(ios.isChecked()){
+                str += ios.getText() + "\n";
+            }
+            if(php.isChecked()){
+                str += php.getText();
+            }
+            Toast.makeText(ImageViewActivity.this, str, Toast.LENGTH_SHORT).show();
         });
-        android.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                if(b) {
-                    Toast.makeText(ImageViewActivity.this, "Checked On", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(ImageViewActivity.this, "Checked Off", Toast.LENGTH_SHORT).show();
-                }
+        android.setOnCheckedChangeListener((compoundButton, b) -> {
+            if(b) {
+                Toast.makeText(ImageViewActivity.this, "Checked On", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(ImageViewActivity.this, "Checked Off", Toast.LENGTH_SHORT).show();
             }
         });
         checkTime = findViewById(R.id.rgCheckTime);
-        checkTime.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                switch (checkedId){
-                    case R.id.sang:
-                        Toast.makeText(ImageViewActivity.this, "Ban chon Sang", Toast.LENGTH_SHORT).show();
-                        break;
-                    case R.id.trua:
-                        Toast.makeText(ImageViewActivity.this, "Ban chon trua", Toast.LENGTH_SHORT).show();
-                        break;
-                    case R.id.chieu:
-                        Toast.makeText(ImageViewActivity.this, "Ban chon chieu", Toast.LENGTH_SHORT).show();
-                        break;
-                }
+        checkTime.setOnCheckedChangeListener((group, checkedId) -> {
+            switch (checkedId){
+                case R.id.sang:
+                    Toast.makeText(ImageViewActivity.this, "Ban chon Sang", Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.trua:
+                    Toast.makeText(ImageViewActivity.this, "Ban chon trua", Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.chieu:
+                    Toast.makeText(ImageViewActivity.this, "Ban chon chieu", Toast.LENGTH_SHORT).show();
+                    break;
             }
         });
 
@@ -124,18 +106,15 @@ public class ImageViewActivity extends AppCompatActivity {
         rbSang = findViewById(R.id.sang);
         rbTrua = findViewById(R.id.trua);
         rbChieu = findViewById(R.id.chieu);
-        btnChectime.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(rbSang.isChecked()){
-                    Toast.makeText(ImageViewActivity.this,rbSang.getText(), Toast.LENGTH_SHORT).show();
-                }
-                if(rbTrua.isChecked()){
-                    Toast.makeText(ImageViewActivity.this, rbTrua.getText(), Toast.LENGTH_SHORT).show();
-                }
-                if(rbChieu.isChecked()) {
-                    Toast.makeText(ImageViewActivity.this, rbChieu.getText(), Toast.LENGTH_SHORT).show();
-                }
+        btnChectime.setOnClickListener(v -> {
+            if(rbSang.isChecked()){
+                Toast.makeText(ImageViewActivity.this,rbSang.getText(), Toast.LENGTH_SHORT).show();
+            }
+            if(rbTrua.isChecked()){
+                Toast.makeText(ImageViewActivity.this, rbTrua.getText(), Toast.LENGTH_SHORT).show();
+            }
+            if(rbChieu.isChecked()) {
+                Toast.makeText(ImageViewActivity.this, rbChieu.getText(), Toast.LENGTH_SHORT).show();
             }
         });
     }

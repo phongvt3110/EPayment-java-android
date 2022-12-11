fcdacdce25b50bf0cf66890552670c96754de25a
phongvt3110 <vtphong651043@gmail.com>
2022-12-11 23:36:31 +0700

    update gitignore

---
+++ app/src/main/java/org/ici/com/MainActivity.java
@@ -0,0 +1,51 @@
+package org.ici.com;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    TextView txtNoidung;
+    Button btnSubmit;
+    EditText userName;
+    Button btnRandomNumber;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, DashboardActivity.class));
+            }
+        });
+        btnRandomNumber.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, RandomNumberActivity.class));
+            }
+        });
+
+    }
+
+    private void init() {
+        txtNoidung = (TextView) findViewById(R.id.txtNoidung);
+        btnSubmit = (Button) findViewById(R.id.btnSubmit);
+        userName = (EditText) findViewById(R.id.username);
+        userName.setText("Android Khoa Pham");
+        btnRandomNumber = findViewById(R.id.btnRandom);
+    }
+    private int randomnum(int min, int max){
+        Random random = new Random();
+        int rannum = random.nextInt(max - min + 1) + min;
+        return rannum;
+    }
+}
